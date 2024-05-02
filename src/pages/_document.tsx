b9680fe3bc0e2d2cf0ffe32f25d9bdd3d1ce524f
divine <divineobi07@gmail.com>
2024-05-02 15:32:57 +0100

    fix: display favicon properly

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/logo.svg" />
         <title>Chatify</title>
       </Head>
       <body>
