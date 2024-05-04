efcd636a86b99558905860b16729b020730cfbc7
divine <divineobi07@gmail.com>
2024-05-04 17:10:20 +0100

    added reset handler for inbox sent button

--- src/components/ChatBox/Chatbox.tsx
+++ src/components/ChatBox/Chatbox.tsx
@@ -1,20 +1,31 @@
-import { useState, useEffect, ChangeEvent } from "react";
+import { useState, useEffect, ChangeEvent, useRef, RefObject } from "react";
 import Microphone from "../../../public/icons/Microphone";
 import Picture from "../../../public/icons/Picture";
 
 const Chatbox = () => {
   const [isClient, setIsClient] = useState(false);
   const [isTyping, setIsTyping] = useState(false);
+  const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
 
   const typingHandler = (e: ChangeEvent) => {
-    // setIsTyping(true);
-    //   console.log(e.target.value);
-    //@ts-ignore
-    if (e.target.value?.trim().length > 0) {
+    if (
+      textAreaRef.current?.value &&
+      textAreaRef.current.value.trim().length > 0
+    ) {
       return setIsTyping(true);
     }
 
     return setIsTyping(false);
+  };
+
+  const sendHandler = () => {
+    if (
+      textAreaRef.current?.value &&
+      textAreaRef.current.value.trim().length > 0
+    ) {
+      textAreaRef.current.value = "";
+      return setIsTyping(false);
+    }
   };
 
   useEffect(() => {
@@ -25,6 +36,7 @@ const Chatbox = () => {
     <div className="bg-white p-[1rem] px-[0.5rem] max-w-[700px] fixed bottom-0 left-[50%] translate-x-[-50%] w-[100%]">
       <div className="px-[0.5rem] flex items-center rounded-[10px] border-[1px] border-gray-300 space-x-[1rem]">
         <textarea
+          ref={textAreaRef}
           onChange={typingHandler}
           name=""
           className="outline-none p-[5px] h-[35px] w-[100%] resize-none placeholder:[vertical-align:middle]"
@@ -48,7 +60,10 @@ const Chatbox = () => {
         )}
         {isTyping && (
           <div className="">
-            <button className="active:bg-blue-600 active:text-white rounded-[7px] px-[0.5rem] text-blue-600 flex items-center justify-center] text-[1rem]">
+            <button
+              onClick={sendHandler}
+              className="active:bg-blue-600 active:text-white rounded-[7px] px-[0.7rem] text-blue-600 flex items-center justify-center] text-[1rem]"
+            >
               Send
             </button>
           </div>
