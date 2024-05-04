ca5cbb6948568c571d6ad3754cb6702b2a3d9a76
divine <divineobi07@gmail.com>
2024-05-04 17:09:56 +0100

    added url navigation progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import type { AppProps } from "next/app";
+import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,6 +18,12 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
       <Component {...pageProps} />
+      <ProgressBar
+        height="4px"
+        color="#000"
+        options={{ showSpinner: false }}
+        shallowRouting
+      />
     </>
   );
 }
