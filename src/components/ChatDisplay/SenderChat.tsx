133d1c65a5c965b3346abecfd0d84e5d707be925
divine <divineobi07@gmail.com>
2024-05-12 15:37:52 +0100

    edit: change drag elstic

--- src/components/ChatDisplay/SenderChat.tsx
+++ src/components/ChatDisplay/SenderChat.tsx
@@ -49,7 +49,7 @@ const SenderChat = () => {
       onDrag={dragFunc}
       whileTap={{ cursor: "grabbing" }}
       whileHover={{ cursor: "grab" }}
-      dragElastic={0.5}
+      dragElastic={0.7}
       onClick={tapFunc}
       className="p-[0.5rem] rounded-[10px] leading-[1.3rem] ml-auto inline-block border-gray-200 border-[1px] w-auto max-w-[200px] [word-break:break-word] space-y-[5px] md:max-w-[300px]"
     >
