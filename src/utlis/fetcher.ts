bb7ba2527dc6ec74430ff249816ed1ee5f22cd20
divine <divineobi07@gmail.com>
2024-05-18 17:08:53 +0100

    feat; added custom fetcher function

--- src/utlis/fetcher.ts
+++ src/utlis/fetcher.ts
@@ -1,6 +1,32 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
+// Define the default fetcher function
 export const fetcher = async (url: string) => {
-  const response = await axios(url);
-  return response.data;
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      // Handle Axios errors
+      throw new Error(error.response.data.message || "An error occurred");
+    } else {
+      // Handle other errors
+      throw new Error("An unexpected error occurred");
+    }
+  }
+};
+
+export const fetcherPost = async (url: string, data: any) => {
+  try {
+    const response = await axios.post(url, data);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      // Handle Axios errors
+      throw new Error(error.response.data.message || "An error occurred");
+    } else {
+      // Handle other errors
+      throw new Error("An unexpected error occurred");
+    }
+  }
 };
