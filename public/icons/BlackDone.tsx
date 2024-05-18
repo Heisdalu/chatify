0a7ccc5bda3f0a5c718ee657b57979ce15cd937a
divine <divineobi07@gmail.com>
2024-05-18 17:11:02 +0100

    fix: added image domain to config

---
+++ public/icons/BlackDone.tsx
@@ -0,0 +1,14 @@
+function BlackDone() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" height={20} width={20} fill="none" viewBox="0 0 24 24">
+      <path
+        fill="#0F1729"
+        fillRule="evenodd"
+        d="M19.707 6.293a1 1 0 010 1.414L10.414 17a2 2 0 01-2.828 0l-4.293-4.293a1 1 0 111.414-1.414L9 15.586l9.293-9.293a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default BlackDone;
