64cd301d3cdc1818590ddb87843af8eefd37639c
divine <divineobi07@gmail.com>
2024-05-24 14:32:52 +0100

    fix: prod breaking

--- src/components/CustomChat/CustomSendingAudio.tsx
+++ src/components/CustomChat/CustomSendingAudio.tsx
@@ -1,4 +1,3 @@
-import DragComponent from "../DragComponent/DragComponent";
 import { Messages } from "@/types";
 import ChatDeliveryStatus from "../ChatDisplay/ChatDeliveryStatus";
 import { useMutation } from "@tanstack/react-query";
@@ -13,25 +12,15 @@ const CustomSendingChat = ({ item }: Props) => {
     mutationFn: (data) => fetcherPost("/api/chat", data),
   });
 
+  console.log('dddd');
+  
+
   return (
-    <DragComponent
-      deactivateDrag="x"
-      className="p-[0.5rem] rounded-[10px] leading-[1.3rem] ml-auto inline-block border-gray-200 border-[1px] w-auto max-w-[200px] [word-break:break-word] space-y-[5px] md:max-w-[300px]"
-    >
-      {item.parentMsgType === "TEXT" || item.parentMsgType === "AUDIO" ? (
-        <div className=" space-y-[0.3rem] rounded-[5px] border-red-400 border-l-[5px] bg-gray-100 p-[0.3rem] px-[0.5rem]">
-          <h1 className="font-[600]">{item?.parentMsgId}</h1>
-          <p className="line-clamp-2 text-[0.8rem] leading-[0.9rem]">
-            {item?.parentMsgContext}
-          </p>
-        </div>
-      ) : (
-        ""
-      )}
+    <div className="p-[0.5rem] rounded-[10px] leading-[1.3rem] ml-auto inline-block border-gray-200 border-[1px] w-auto max-w-[200px] [word-break:break-word] space-y-[5px] md:max-w-[300px]">
       <p className="text-[0.9rem]"> {item?.msgContext}</p>
 
       <ChatDeliveryStatus sentTimestamp={item.sentAt} isSeen={item.isSeen} />
-    </DragComponent>
+    </div>
   );
 };
 export default CustomSendingChat;
